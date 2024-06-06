21856a72557caac41cc82c1c4e17cc27a78000ea
Bruno <bruno.rp45@gmail.com>
2024-06-06 16:19:27 -0300

    Aplicando temas claro e escuro; não está funcionando ainda

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 let currentIndex = 0;
 const items = document.querySelectorAll('.carousel-item');
 const totalItems = items.length;
+const themeToggle = document.getElementById('themeToggle');
 
+
+themeToggle.addEventListener('click', () => {
+
+    /// Toggle themes on the <html> element
+    document.documentElement.classList.toggle('dark-theme');
+    document.documentElement.classList.toggle('light-theme');
+
+    // Toggle themes specifically for the header or other elements if needed
+    let elementsToToggle = document.querySelectorAll('.header, .other-class');
+    elementsToToggle.forEach(element => {
+        element.classList.toggle('dark-theme');
+        element.classList.toggle('light-theme');
+    });
+
+    // Update button text based on the current theme
+    this.textContent = document.documentElement.classList.contains('dark-theme') ? 'Tema Claro' : 'Tema Escuro';
+
+    console.log(document.documentElement.classList);
+});
+// Funções
 function updateCarousel() {
     items.forEach((item, index) => {
         if (index === currentIndex) {
