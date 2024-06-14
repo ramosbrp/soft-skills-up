12ae22124ec5a7a4baeb4376554353205ed13d23
Bruno <bruno.rp45@gmail.com>
2024-06-14 18:53:09 -0300

    ✨ feat(Buscando pergunta do banco):
    Buscando a pergunta do banco ao entrar na tela do app

---
+++ controllers/app.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+$dotent = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotent->load();
+
+// Agora você pode acessar as variáveis como variáveis de ambiente
+$dbHost = $_ENV['DB_HOST'];
+$dbDatabase = $_ENV['DB_DATABASE'];
+$dbUser = $_ENV['DB_USERNAME'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+
+// print_r($_ENV); // Lista todas as variáveis de ambiente para testar apenas
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:$dbHost; Database = $dbDatabase", $dbUser, $dbPassword);
+} catch (Exception $e) {
+    print ("Error connecting to SQL Server.");
+    die(print_r($e->getMessage()));
+}
+
+
+$level = isset($_POST['level']) ? $_POST['level'] : 1;
+
+$query = "SELECT id, question_text FROM questions WHERE level = :level";
+$stmt = $conn->prepare($query);
+$stmt->bindParam(':level', $level, PDO::PARAM_INT);
+$stmt->execute();
+
+$question = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($question) {
+
+    $questionId = $question["id"];
+    $options_query = "SELECT id, option_text FROM options WHERE question_id =:questionId";
+    $stmt = $conn->prepare($options_query);
+    $stmt->bindParam(":questionId", $questionId, PDO::PARAM_INT);
+    $stmt->execute();
+    
+    $options = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $responseArray = [
+        'success' => true,
+        'question' => $question,
+        'options' => $options
+    ];
+} else {
+    $responseArray = ['success' => false, 'message' => "Pergunta não encontrada"];
+}
+
+// Retorna a resposta em formato JSON
+echo json_encode($responseArray);
--- js/app.js
+++ js/app.js
@@ -1,24 +1,112 @@
+/**
+ * Variáveis
+ */
+
+const quizForm = document.getElementById('quiz-form');
+const quizOptions = Array.from(document.querySelectorAll('.quiz-option'));
+const scoreElement = document.getElementById('score');
+const questionText = document.getElementById('question-text');
+const optionsContainer = document.getElementById('options-container');
+var score = 0;
+
 document.addEventListener("DOMContentLoaded", () => {
-    var enviarResposta = document.getElementById('enviar-resposta');
-    var quizOptions = Array.from(document.querySelectorAll('.quiz-option'));
-    var scoreElement = document.getElementById('score');
 
+    quizForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // enviarResposta_(event.target);
 
-
-    enviarResposta.addEventListener('click', () => {
-        var score = 0;
-
-
-        quizOptions.forEach((option) => {
-            if (option.checked) {
-                var parentDiv = option.closest('.app-opcoes');
-
-                var optionScore = parseInt(parentDiv.getAttribute('data-score'));
-                score = optionScore;
-
-            }
-        })
-        scoreElement.textContent = score;
+        console.log('ok');
     });
+
+
+    carregarPergunta();
 })
+
+
+
+
+/**
+ * Funções
+ */
+//Função para carregar a pegunta
+const carregarPergunta = async () => {
+    var level = 1;
+    try {
+        const response = await fetch('../controllers/app.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'level=' + level
+        });
+        if (!response.ok)
+            throw new Error("Error");
+
+        const data = await response.json();
+
+        mostrarPergunta(data);
+        toastr.success('Pergunta pronta!!');
+
+    } catch (error) {
+        console.error('Error: ', error);
+        toastr.error('An erro occurred during response');
+    }
+}
+
+const mostrarPergunta = (questionData) => {
+    questionText.textContent = questionData.question.question_text;
+
+    optionsContainer.innerHTML = '';
+
+    questionData.options.forEach(option => {
+
+        
+        
+        const optionElement = document.createElement('input');
+        const label = document.createElement('label');
+        const optionContainer = document.createElement('div');
+        const p = document.createElement('p');
+
+        //Cria um elemento de rádio para cada opção
+        console.log(option)
+        optionElement.type = 'radio';
+        optionElement.name = 'quiz-option';
+        optionElement.value = option.option_id;
+
+        //Cria um label para o input
+        label.textContent = option.option_text;
+        label.htmlFor = option.option_id;
+
+        p.textContent = option.option_text;
+
+        //Cria um container para a opção
+        optionContainer.className = 'app-opcoes';
+
+        optionContainer.appendChild(optionElement);
+        optionContainer.appendChild(label);
+        optionContainer.appendChild(p);
+        optionsContainer.appendChild(optionContainer);
+    });
+}
+
+const enviarResposta_ = async () => {
+    quizOptions.forEach((option) => {
+        if (option.checked) {
+            var parentDiv = option.closest('.app-opcoes');
+
+            var optionScore = parseInt(parentDiv.getAttribute('data-score'));
+            score = optionScore;
+
+        }
+    })
+    scoreElement.textContent = score;
+    try {
+        const response = await fetch('../controllers/app.php', {
+            //logica para obter 
+        });
+    } catch (error) {
+        console.error('Error: ', error);
+        toastr.error('An erro occurred during response');
+    }
+}
 
