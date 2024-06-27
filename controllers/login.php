701310222b5863e5c10ca64f8741cc8ba754284b
Bruno <bruno.rp45@gmail.com>
2024-06-26 21:30:10 -0300

    Adicione telemetria para Application Insights; Refatoração da controller seguindo boas práticas; Condicional para pegar variávle de ambiente.

--- controllers/login.php
+++ controllers/login.php
@@ -3,57 +3,82 @@ session_start();
 
 require_once '../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
+use ApplicationInsights\Telemetry_Client;
 
-// Agora você pode acessar as variáveis como variáveis de ambiente
-$dbHost = getenv('DB_HOST');
-$dbDatabase = getenv('DB_DATABASE');
-$dbUser = getenv('DB_USERNAME');
-$dbPassword = getenv('DB_PASSWORD');
 
-// print_r($_ENV); // Lista todas as variáveis de ambiente para testar apenas
-
-try {
-    $conn = new PDO("sqlsrv:server = tcp:$dbHost; Database = $dbDatabase", $dbUser, $dbPassword);
-} catch (Exception $e) {
-    print ("Error connecting to SQL Server.");
-    die(print_r($e->getMessage()));
+// Verificar se está em ambiente de desenvolvimento
+$appEnv = getenv('APP_ENV');
+if ($appEnv == 'development') {
+    // Carregar variáveis de ambiente do arquivo .env apenas em desenvolvimento
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->load();
 }
 
+$instrumentalKey = $_ENV['INSTRUMENTATION_KEY'];
 
-// Consulta SQL para verificar o usuário
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $login = $_POST['login'];
-    $senha = $_POST['senha'];
+// Configuração do Application Insights
+$client = new Telemetry_Client();
+$client->getContext()->setInstrumentationKey($instrumentalKey);
 
-    // Consulta SQL para verificar o usuário
-    $query = "SELECT * FROM usuario WHERE login = :login";
-    $stmt = $conn->prepare($query);
-    $stmt->bindParam(':login', $login, PDO::PARAM_STR);
-    $stmt->execute();
+// Função para criar conexão com o banco de dados
+function createDatabaseConnection()
+{
+    $dbHost = $_ENV['DB_HOST'];
+    $dbDatabase = $_ENV['DB_DATABASE'];
+    $dbUser = $_ENV['DB_USERNAME'];
+    $dbPassword = $_ENV['DB_PASSWORD'];
 
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $num_rows = count($results);
+    global $client;  // Usar o cliente de telemetria global
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:$dbHost; Database = $dbDatabase", $dbUser, $dbPassword);
 
-    if ($num_rows > 0) {
-        //Usuário encontrado
-        $user = $results[0];
-        $hash_senha = $user['senha'];
-
-        if (password_verify($senha, $hash_senha)) {
-            $_SESSION['loggedin'] = true;
-            $_SESSION['username'] = $login;
-
-            $responseArray = ['success' => true, 'message' => 'Login bem-sucedido!', 'username' => $login];
-            echo json_encode($responseArray);
-
-        } else {
-            $responseArray = ['success' => false, 'message' => 'Senha incorreta.'];
-            echo json_encode($responseArray);
-        }
-    } else {
-        $responseArray = ['success' => false, 'message' => "Usuário $login não encontrado. "];
-        echo json_encode($responseArray);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        $client->trackException($e);
+        error_log("Error connecting to SQL Server: " . $e->getMessage());
+        die("Error connecting to SQL Server.");
     }
 }
+
+// Função para validar o login do usuário
+function validateUser($login, $password)
+{
+    global $client;  // Usar o cliente de telemetria global
+    $conn = createDatabaseConnection();
+
+    try {
+        $query = "SELECT * FROM usuario WHERE login = :login";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':login', $login, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if (count($results) > 0) {
+            $user = $results[0];
+            if (password_verify($password, $user['senha'])) {
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $login;
+                return ['success' => true, 'message' => 'Login bem-sucedido!', 'username' => $login];
+            } else {
+                return ['success' => false, 'message' => 'Senha incorreta.'];
+            }
+        } else {
+            return ['success' => false, 'message' => "Usuário $login não encontrado."];
+        }
+    } catch (PDOException $e) {
+        $client->trackException($e);
+        error_log("Database query error: " . $e->getMessage());
+        return ['success' => false, 'message' => 'Erro ao acessar o banco de dados.'];
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_STRING);
+    $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+
+    $responseArray = validateUser($login, $senha);
+    echo json_encode($responseArray);
+}
+?>
