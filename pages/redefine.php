15e06b50a2c5957b62751953858e9f218c2fb833
Bruno <bruno.rp45@gmail.com>
2024-06-27 17:45:31 -0300

    Ajuste em recsenha e redefine

--- pages/redefine.php
+++ pages/redefine.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,59 +9,70 @@
     <link rel="stylesheet" type="text/css" href="../css/redefine.css">
 
 </head>
+
 <body>
     <h1>Redefinição de Senha</h1>
     <div class="input-entrada">
-    <form action="redefine.php" method="post">
-        <label class ="label" for="cod_rec">Código de Recuperação:</label><br>
-        <input autocomplete="off" class="input" type="text" id="cod_rec" name="cod_rec"><br><br>
+        <form action="redefine.php" method="post">
+            <label class="label" for="cod_rec">Código de Recuperação:</label><br>
+            <input autocomplete="off" class="input" type="text" id="cod_rec" name="cod_rec"><br><br>
 
-        <label class ="label" for="nova_senha">Nova Senha:</label><br>
-        <input autocomplete="off" class="input" type="password" id="nova_senha" name="nova_senha"><br><br>
+            <label class="label" for="nova_senha">Nova Senha:</label><br>
+            <input autocomplete="off" class="input" type="password" id="nova_senha" name="nova_senha"><br><br>
 
-        <button class="button" type="submit" name="redefinir" value="Redefinir Senha">Redefinir a Senha</button>
-    </form>
+            <button class="button" type="submit" name="redefinir" value="Redefinir Senha">Redefinir a Senha</button>
+        </form>
 </body>
+
 </html>
 
 <?php
 session_start();
+require_once '../vendor/autoload.php';
 
 // Verifique se o formulário foi enviado
 if (isset($_POST['redefinir'])) {
-    // Conexão com o banco de dados
-    $hostname = "localhost";
-    $bancodedados = "projeto";
-    $usuario = "root";
-    $senha = "softskills@123";
+    // Apenas carregar dotenv em ambientes que não são de produção
+    if (getenv('APP_ENV') === 'development') {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+        $dotenv->load();
+    }
 
-    $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
-    if ($mysqli->connect_errno) {
-        echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-        exit();
+    $dbHost = getenv('DB_HOST');
+    $dbDatabase = getenv('DB_DATABASE');
+    $dbUser = getenv('DB_USERNAME');
+    $dbPassword = getenv('DB_PASSWORD');
+
+    try {
+        $conn = new PDO("sqlsrv:server=$dbHost;Database=$dbDatabase", $dbUser, $dbPassword);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Falha ao conectar: " . $e->getMessage();
     }
 
     // Obtenha o código de recuperação e a nova senha digitados pelo usuário
     $cod_rec = $_POST['cod_rec'];
-    $nova_senha = $_POST['nova_senha'];
+    $nova_senha = password_hash($_POST['nova_senha'], PASSWORD_BCRYPT);
 
     // Verifique se o código de recuperação e a nova senha não estão vazios
     if (!empty($cod_rec) && !empty($nova_senha)) {
         // Consulta para obter o e-mail do usuário com base no código de recuperação
-        $query = $mysqli->prepare("SELECT email FROM usuario WHERE cod_rec = ?");
-        $query->bind_param("s", $cod_rec);
-        $query->execute();
-        $result = $query->get_result();
+        $query = "SELECT email FROM usuario WHERE cod_rec = :cod_rec";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':cod_rec', $cod_rec, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if ($result->num_rows > 0) {
+        if ($result) {
             // Usuário encontrado, atualize a senha
-            $row = $result->fetch_assoc();
-            $email = $row['email'];
+            $email = $result['email'];
 
             // Atualize a senha do usuário no banco de dados
-            $update_query = $mysqli->prepare("UPDATE usuario SET senha = ? WHERE email = ?");
-            $update_query->bind_param("ss", $nova_senha, $email);
-            $update_query->execute();
+            $update_query = "UPDATE usuario SET senha = :nova_senha WHERE email = :email";
+            $update_stmt = $conn->prepare($update_query);
+            $update_stmt->bindParam(':nova_senha', $nova_senha, PDO::PARAM_STR);
+            $update_stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $update_stmt->execute();
 
             echo "Senha redefinida com sucesso!";
         } else {
